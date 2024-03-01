9050afa2dbf147e8f058d908b9f8bf5e40462bc2
typicalcameron <53804604+typicalcameron@users.noreply.github.com>
2024-02-29 22:40:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Write-2Log
+ 
---
+++ Write-2Log.ps1
@@ -0,0 +1,61 @@
+﻿function Write-2Log {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("DEBUG","INFO","WARNING","ERROR")]
+        [string]$SeverityLevel,
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+
+    $GetDate = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+    $Path = $env:TEMP
+    $FileName = "$(($MyInvocation.MyCommand.Name).Split(".ps1")[0]).log"
+    $ErrorActionPreference = "SilentlyContinue"
+
+    $PathFile = "$($Path)\$($FileName)"
+    $GetFilePath = Get-Item -Path $PathFile -ErrorAction $ErrorActionPreference
+
+    if ($GetFilePath -eq $null)
+    {
+        try 
+        {
+            New-Item -Path $Path -Name $FileName | Out-Null
+        }
+        catch
+        {
+            $_.Exception.Message
+        }
+    }
+
+    switch ($InfoLevel)
+    {
+        "DEBUG" 
+        {
+             $Value = "$GetDate - $SeverityLevel - $Message"
+             Write-Output $Value
+             Add-Content -Path $PathFile -Value $Value
+             Start-Sleep -Seconds 1
+        }
+        "INFO" 
+        {
+             $Value = "$GetDate - $SeverityLevel - $Message"
+             Write-Output $Value
+             Add-Content -Path $PathFile -Value $Value
+             Start-Sleep -Seconds 1
+        }
+        "WARNING" 
+        {
+             $Value = "$GetDate - $SeverityLevel - $Message"
+             Write-Output $Value
+             Add-Content -Path $PathFile -Value $Value
+             Start-Sleep -Seconds 1        
+        }
+        "ERROR"
+        {
+             $Value = "$GetDate - $SeverityLevel - $Message"
+             Write-Output $Value
+             Add-Content -Path $PathFile -Value $Value
+             Start-Sleep -Seconds 1            
+        }
+    }
+}
